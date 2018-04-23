540f14ffde1dd9c8418f056e18239ab8efa4d446
mikey96g <mikey96g@hotmail.com>
2018-04-23 15:00:14 +0100

    Update caPrac.py

--- caPrac.py
+++ caPrac.py
@@ -6,7 +6,7 @@ def convert(currencey,value):
 
 
 def re_convert(currencey,value):
-	curr= value * 1/curr_xe[currencey]
+	curr = value * 1/curr_xe[currencey]
 	return curr
 
 
@@ -14,3 +14,29 @@ temp = convert('USD',10)
 
 print(convert('USD',10))
 print(re_convert('USD',temp))
+
+bankAccount = []
+
+def debit(value):
+	bankAccount.append(value)
+	
+def credit(value):
+	try:
+		if value < 0:
+			bankAccount.append(value)
+	except ValueError:
+		val = print("That is an error must be minus")
+		
+def balAccount(bankAccount):
+	if len(bankAccount)==0:
+		return 0;
+	else:
+		return bankAccount[0] + balAccount(bankAccount[1:])
+
+
+print(len(bankAccount))
+credit(-23)
+debit(50)
+
+print(balAccount(bankAccount))	
+
